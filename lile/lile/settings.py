f770e87d2ab3cc938acfd7d776bb04491a075b15
akiokio <akio.xd@gmail.com>
2015-11-30 16:09:36 -0200

    changed static file

--- lile/lile/settings.py
+++ lile/lile/settings.py
@@ -116,7 +116,7 @@ STATIC_URL = '/static/'
 #     os.path.join(BASE_DIR, "static"),
 # )
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "collected_static")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
